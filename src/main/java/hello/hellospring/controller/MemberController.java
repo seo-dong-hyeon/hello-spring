3af257172f79985f3cdd1207eec50205ebe1aa24
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2023-03-05 06:01:47 +0900

    회원 관리 예제 - 회원 조회 기능 추가

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -30,5 +33,14 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        // 모든 멤버 리스트를 모델에 담아서 화면(뷰 템플릿)에 넘김
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
